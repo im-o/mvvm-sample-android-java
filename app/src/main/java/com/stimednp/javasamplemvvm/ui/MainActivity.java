30f70f16dd9af0aca5ef4b3b8b967b9d675e4485
im-o <frizgt@gmail.com>
2020-08-03 23:43:29 +0800

    :zap: add MovieAdapter and pass data from MaintActivity to RecyclerViewAdapter

--- app/src/main/java/com/stimednp/javasamplemvvm/ui/MainActivity.java
+++ app/src/main/java/com/stimednp/javasamplemvvm/ui/MainActivity.java
@@ -1,14 +1,17 @@
 package com.stimednp.javasamplemvvm.ui;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.stimednp.javasamplemvvm.databinding.ActivityMainBinding;
+import com.stimednp.javasamplemvvm.model.MovieList;
 import com.stimednp.javasamplemvvm.viewmodel.MovieViewModel;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -24,8 +27,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViewModel() {
         MovieViewModel viewModel = new ViewModelProvider(this).get(MovieViewModel.class);
-        viewModel.getMovies().observe(this, movieLists -> {
-            Log.e("INI", "DATA -> " + movieLists.size());
-        });
+        viewModel.getMovies().observe(this, this::initAdapter);
+    }
+
+    private void initAdapter(ArrayList<MovieList> movieLists) {
+        MovieAdapter movieAdapter = new MovieAdapter();
+        movieAdapter.setMovieList(movieLists);
+        movieAdapter.notifyDataSetChanged();
+        binding.rvMovie.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMovie.setAdapter(movieAdapter);
     }
 }
