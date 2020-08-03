f5d3b05062f75cabead94d91edaf14f2efe1c183
im-o <frizgt@gmail.com>
2020-08-03 18:23:24 +0800

    :zap: set ViewModel on MainActivity and get data Movies

--- app/src/main/java/com/stimednp/javasamplemvvm/ui/MainActivity.java
+++ app/src/main/java/com/stimednp/javasamplemvvm/ui/MainActivity.java
@@ -1,16 +1,31 @@
 package com.stimednp.javasamplemvvm.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import com.stimednp.javasamplemvvm.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.stimednp.javasamplemvvm.databinding.ActivityMainBinding;
+import com.stimednp.javasamplemvvm.viewmodel.MovieViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initViewModel();
+    }
+
+    private void initViewModel() {
+        MovieViewModel viewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+        viewModel.getMovies().observe(this, movieLists -> {
+            Log.e("INI", "DATA -> " + movieLists.size());
+        });
     }
 }
